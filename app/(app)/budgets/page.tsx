cd7441fd73c5446630b2f1d4a2c6403542556efc
Berrones Edwin <Berrones.edwin@hotmail.com>
2026-09-07 14:31:40 -0600

    fix remove export const instant = false;

--- app/(app)/budgets/page.tsx
+++ app/(app)/budgets/page.tsx
@@ -1,9 +1,6 @@
 import { PiggyBank } from "lucide-react"
 import { ComingSoon } from "@/components/shared/coming-soon"
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
 
 export default function BudgetsPage() {
   return (
